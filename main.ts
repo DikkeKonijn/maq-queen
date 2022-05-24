8a2b0a45a6f06b3a921153a7a12afa85b84631ad
DikkeKonijn <97957431+DikkeKonijn@users.noreply.github.com>
2022-05-24 09:11:03 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,6 @@
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+basic.forever(function () {
+	
+})
